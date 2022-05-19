013e002ff8070ee5d39ce4e75134c5e5825b7881
Agung Indrawan <agung_indrawan_x@telkomsel.co.id>
2022-05-19 20:55:35 +0700

    object

--- src/function.ts
+++ src/function.ts
@@ -15,3 +15,17 @@ const multiply = (val1 :number, val2:number) : number =>{
 }
 
 console.log(multiply(5,5))
+
+const noreturn = () : void =>{
+   console.log("no return")
+}
+
+noreturn();
+
+//function as types
+type Tambah = (val1:number, val2:number) => number;
+
+const Add : Tambah = (val1:number,val2:number) :number =>{
+   return val1 * val2;
+}
+console.log(Add(2,5))
---
+++ src/object.ts
@@ -0,0 +1,10 @@
+//object
+
+type User = {
+   name:string,
+   age:number
+}
+let user:User = {
+   name:"Nusendra",
+   age:321
+}
